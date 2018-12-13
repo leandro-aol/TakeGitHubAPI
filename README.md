97752eee627222a58f48c4bb2c231da6972fd0aa
Leandro Andrade de Oliveira <leandro@live.com.my>
2018-12-13 21:08:05 -0200

    Detalhes finais do projeto.
    * Comentários no código
    * Arrumando o arquivo README

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # TakeGitHubAPI
 API intermediária para consumir dados da API do GitHub
 
-Faça o download e instale o [Node.js](https://nodejs.org/en/download/)
+## Vamos lá!
+1) Faça o download e instale o [Node.js](https://nodejs.org/en/download/) corresponde à sua plataforma.
 
-Crie e entre na pasta do projeto
-* npm init
-* npm i -g nodemon
+2) Crie e entre na pasta do projeto
+    * npm init
 
-_-g significa que o nodemon será instalado de forma global_
+3) Instalar o [Restler](https://github.com/danwrong/restler) e o [Express](https://expressjs.com/pt-br/)
+    * npm i restler --save
+    * npm - express --save
 
-Instalar a dependência *[Restler](https://github.com/danwrong/restler)*
-* npm i restler --save
+        _--save irá salvar a dependência no seu arquivo `package.json`_
+
+4) Instale também o Nodemon.    
+    * npm i -g nodemon
+
+        _-g significa que o nodemon será instalado de forma global_
+
+        Ele falicita bastante para não ter que parar e executar novamente o servidor a cada save.
+
+## Pequenos lembretes!
 
 Para executar o servidor
 * nodemon api
 
+Para instalar as dependências do arquivo `package.json`
+* npm install 
+
 Heroku
 * heroku login
+* heroku create
 * git push heroku master
--- api.js
+++ api.js
@@ -1,27 +1,39 @@
+// Importa as dependências do projeto
 const rest = require('restler');
-
 const express = require('express');
 const api = express();
 
+// Define a porta através da variável de ambiente para o uso no Heroku OU utiliza a porta 3001
+const porta = process.env.PORT || 3001;
+
+// Endereço principal da API do GitHUb para o usuário da Take
 const mainAddr = 'https://api.github.com/users/takenet'
+
+// Endereço secundário de pesquisa dos repositórios em que a Take é o dono, ordenados por data de criação, de forma crescente
 const searchAddr = '/repos?type=owner&sort=created&direction=asc';
 
-var porta = process.env.PORT || 3001;
-
+// Rota para uma requisição GET no endereço /takenet/Csharp
 api.get('/takenet/CSharp/', (req, res) => {
+
+    // Realiza uma requisição GET na API do GitHub
     rest.get(`${mainAddr}${searchAddr}`).on('complete', function(result) {
+        
+        // Dicionário dos repositórios que passaram pelas condições de pesquisa
         var repositorios = {};
         var index = 1;
 
+        // Verifica se o retorno da requisição e tenta novamente caso dê erro
         if (result instanceof Error) {
             console.log('Erro: ', result.message);
-            this.retry(5000);   // Tenta novamente em 5 segundos
+            this.retry(5000);
         }
         else {
             for (var repoIndex in result) { // Loop para cada repositório
                 
+                // Condição para verificar se o repositório é da linguagem C#
                 if (result[repoIndex].language === 'C#' ) {
 
+                    // Adiciona no dicionário as informações necessárias do repositório
                     repositorios["Repo" + index] = {
                         "titulo" : result[repoIndex].full_name,
                         "subtitulo" : result[repoIndex].description,
@@ -34,11 +46,13 @@ api.get('/takenet/CSharp/', (req, res) => {
             }
         }
 
+        // Define o tipo de resposta para o formato JSON e envia o dicionário
         res.type('application/json');
         res.json(repositorios);
     });
 });
 
+// Habilita a API para ficar esperando por uma requisição
 api.listen(porta, () => {
     console.log(`Servidor rodando em http://localhost:${porta}`)
     console.log('Para desligar o servidor: ctrl + c')
